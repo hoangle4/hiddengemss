c5c1b8fd789b2db6296b7e1ca2b453a58831cdf3
Hoang Le <hoang.le4@pcc.edu>
2019-09-30 21:09:32 -0700

    commit modal open and close

--- client/src/views/Map/index.js
+++ client/src/views/Map/index.js
@@ -2,19 +2,33 @@ import React, { useEffect, useState, useContext } from 'react';
 import { renderMap } from '../../utils';
 import { Redirect } from 'react-router-dom';
 import mapContext from '../../context/map/MapContext';
+import AddLocationForm from './AddLocationForm';
+import './Map.css';
 const Map = () => {
+	const [ openModal, setOpenModal ] = useState(false);
 	const [ map, setMap ] = useState();
 	const [ clickedLocation, setClickedLocation ] = useState({});
+	useEffect(
+		() => {
+			setOpenModal(openModal);
+		},
+		[ openModal ]
+	);
+
 	const MapContext = useContext(mapContext);
 
 	useEffect(() => {
 		if (MapContext.center.length === 0) return;
 		setMap(renderMap(MapContext.center));
 	}, []);
-	if (map) map.on('click', (e) => setClickedLocation(e.latLng));
+	if (map)
+		map.on('click', (e) => {
+			setClickedLocation(e.latLng);
+			setOpenModal(!openModal);
+		});
 	if (MapContext.center.length === 0) return <Redirect to='/' />;
 
-	return <div />;
+	return <AddLocationForm openModal={openModal} />;
 };
 
 export default Map;
