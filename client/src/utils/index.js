d34f7d5f4ac147809372eef9c183b97dcf2f54b8
hoangle4 <hoang.le4@pcc.edu>
2019-10-02 23:11:53 -0700

    commit box

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,15 +1,15 @@
 import { Map } from 'mapbox-gl';
-export const renderMap = (latLng) => {
-	var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+export const renderMap = latLng => {
+  var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
-	mapboxgl.accessToken = process.env.REACT_APP_MAP_BOX_API;
-	const m = document.getElementById('root');
-	m.style.width = '100vw';
-	m.style.height = '100vh';
-	return new Map({
-		container: 'root',
-		style: 'mapbox://styles/mapbox/streets-v11',
-		zoom: 12,
-		center: latLng
-	});
+  mapboxgl.accessToken = process.env.REACT_APP_MAP_BOX_API;
+  const m = document.getElementById('root');
+  m.style.width = '100vw';
+  m.style.height = '100vh';
+  return new Map({
+    container: 'root',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    zoom: 12,
+    center: latLng
+  });
 };
--- client/src/views/Map/index.js
+++ client/src/views/Map/index.js
@@ -5,30 +5,25 @@ import mapContext from '../../context/map/MapContext';
 import AddLocationForm from './AddLocationForm';
 import './Map.css';
 const Map = () => {
-	const [ openModal, setOpenModal ] = useState(false);
-	const [ map, setMap ] = useState();
-	const [ clickedLocation, setClickedLocation ] = useState({});
-	useEffect(
-		() => {
-			setOpenModal(openModal);
-		},
-		[ openModal ]
-	);
+  const [openModal, setOpenModal] = useState(false);
+  const [map, setMap] = useState();
+  const [clickedLocation, setClickedLocation] = useState({});
 
-	const MapContext = useContext(mapContext);
+  const MapContext = useContext(mapContext);
 
-	useEffect(() => {
-		if (MapContext.center.length === 0) return;
-		setMap(renderMap(MapContext.center));
-	}, []);
-	if (map)
-		map.on('click', (e) => {
-			setClickedLocation(e.latLng);
-			setOpenModal(!openModal);
-		});
-	if (MapContext.center.length === 0) return <Redirect to='/' />;
+  useEffect(() => {
+    if (MapContext.center.length === 0) return;
 
-	return <AddLocationForm openModal={openModal} closeModal={setOpenModal} />;
+    renderMap(MapContext.center).on('click', e => {
+      MapContext.setCenter(e.lngLat);
+
+      setOpenModal(!openModal);
+    });
+  }, []);
+
+  if (MapContext.center.length === 0) return <Redirect to='/' />;
+
+  return <AddLocationForm openModal={openModal} closeModal={setOpenModal} />;
 };
 
 export default Map;
