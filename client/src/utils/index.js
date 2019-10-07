c449b93a4a71d976acd3490eab3e2758714abdd5
hoangle4 <hoang.le4@pcc.edu>
2019-10-06 22:19:41 -0700

    generate popup

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,4 @@
-import { Map, Marker } from 'mapbox-gl';
+import { Map, Marker, Popup } from 'mapbox-gl';
 import axios from 'axios';
 export const renderMap = latLng => {
   var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
@@ -15,8 +15,30 @@ export const renderMap = latLng => {
   });
 };
 
-export const renderMarker = (latLng, map) => {
-  return new Marker().setLngLat(latLng).addTo(map);
+export const renderMarker = (latLng, map, popup) => {
+  return new Marker()
+    .setLngLat(latLng)
+    .setPopup(popup)
+    .addTo(map);
+};
+
+export const setPopup = html => {
+  return new Popup({ closeButton: true }).setHTML(html);
+};
+
+export const generatePopupHTML = (name, desc, story, photo, id) => {
+  return `<div className='card'>
+            <img src='${photo}' className='card-img-top' alt='${name}' />
+            <div className='card-body'>
+              <h5 className='card-title'>${name}</h5>
+              <small className='card-text text-muted'>${desc}</small>
+              <p className='card-text'>${story}</p>
+              <a href='/gem/${id}' className='btn'>
+                Read more.
+              </a>
+            </div>
+          </div>
+          `;
 };
 
 export const setAuthToken = authToken => {
